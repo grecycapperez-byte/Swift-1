9870682ed808aeaed03fa93b7c0057f7fe51ab82
Grey <grecycapperez@gmail.com>
2026-03-05 23:14:49 -0600

    Add basic Swift program with variables and print statements

---
+++ prac1.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// 1. Hola Mundo
+print("¡Hola, mundo!")
+
+// 2. Variables (se pueden cambiar) y Constantes (no se pueden cambiar)
+var nombre = "Usuario"
+let añoActual = 2026
+print("Hola \(nombre), bienvenido al año \(añoActual).")
+s y mide \(altura) metros.")
+
+
