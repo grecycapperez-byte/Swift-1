6aa8b1c57266881e74d47e27a0fefbdc3144a319
Grey <grecycapperez@gmail.com>
2026-03-26 23:59:46 -0600

    Add basic Swift program with personal details

---
+++ practica3.swift
@@ -0,0 +1,15 @@
+let nombre = "Dulce Grecia Perez Capilla"
+var edad = 17
+let lugar_de_nacimiento = "San Pablo del Monte Tlaxcala"
+let leagradaswift = true
+
+if leagradaswift  {
+    print("Si estás feliz aprendiendo Swift🕺")
+} else {
+    print("Pues ya ni modo ve a dormir 😴")
+}
+
+let pi: Double = 3.14159
+
+let añoActual: Int = 2026
+var edadFutura= edad + 5
